7bd691e7a5ea6ff6039b03191305ab907432db81
davkimfray <davkimfray@gmail.com>
2019-02-06 11:28:48 +0300

    Subject and Class Registration Added

--- app/src/main/java/com/example/davkimfray/shuta4/staffView.java
+++ app/src/main/java/com/example/davkimfray/shuta4/staffView.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 
-public class Registration extends AppCompatActivity {
+ class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
